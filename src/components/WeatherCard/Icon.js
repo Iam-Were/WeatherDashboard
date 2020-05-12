863a5c54debd2346ffb0612396186bcbc80a20c9
Tony Were <iamwere1.0@gmail.com>
2020-05-12 12:55:29 +0100

    Weathercards are complete

---
+++ src/components/WeatherCard/Icon.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+const Icon = (props) => {
+  const Icon = styled.img`
+    width: 40%;
+  `;
+
+  return <img className="icon" src="./img/overcast.png" alt="Weather Icon" />;
+};
+
+export default Icon;

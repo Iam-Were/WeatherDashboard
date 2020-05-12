f08f147682bb59434ba0c14bf38ba8751aa5d6b0
Tony Were <iamwere1.0@gmail.com>
2020-05-12 14:18:17 +0100

    Stateless weathercard updated with prop drilling.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import React from "react";
-import WeatherCard from "./components/WeatherCard/component";
-import { CardDeck, CardGroup } from "reactstrap";
+import React from 'react';
+import WeatherCard from './components/WeatherCard/component';
+import { CardDeck, CardGroup } from 'reactstrap';
 
-import "./App.css";
+import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <WeatherCard temp={-15} />
-      <WeatherCard temp={20} />
-      <WeatherCard temp={39} />
-    </div>
-  );
+	return (
+		<div className="App">
+			<WeatherCard temp={-15} condition="Clear" />
+			<WeatherCard temp={20} condition="Tornado" />
+			<WeatherCard temp={39} condition="Fog" />
+		</div>
+	);
 }
 
 export default App;

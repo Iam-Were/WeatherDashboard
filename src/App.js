717c810c1f2e9df5e43a7ddcf9ae1d06107a39ca
Tony Were <iamwere1.0@gmail.com>
2020-05-12 12:52:51 +0100

    Weathercard components complete.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import WeatherCard from "./components/WeatherCard/component";
+import { CardDeck, CardGroup } from "reactstrap";
+
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <WeatherCard temp={-15} />
+      <WeatherCard temp={20} />
+      <WeatherCard temp={39} />
     </div>
   );
 }
